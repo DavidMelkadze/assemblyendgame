0a79ba36d542e097daf51a8bc0cbb4a727847a4b
DavidMelkadze <davidmelkadze23@gmail.com>
2026-04-02 14:31:32 +0400

    feat: implement core layout and keyboard logic with letter state feedback

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,74 @@
 import { useState } from "react";
+import clsx from "clsx";
+import { languages } from "./languages.js";
 
-export default function App() {
+export default function AssemblyEndgame() {
+  const [currentWord, setCurrentWord] = useState("react");
+
+  const [guessedLetters, setGuessedLetters] = useState([]);
+
+  const alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+  const addGuessedLetter = (letter) => {
+    setGuessedLetters((prevLetters) =>
+      prevLetters.includes(letter) ?
+      prevLetters : 
+      [...prevLetters, letter]
+    );
+  };
+
+  const languageElements = languages.map((language) => {
+    return (
+      <span
+        className="chip"
+        key={language.name}
+        style={{
+          backgroundColor: language.backgroundColor,
+          color: language.color,
+        }}
+      >
+        {language.name}
+      </span>
+    );
+  });
+
+  const letterElements = currentWord.split("").map((letter, index) => {
+    return <span key={index}>{letter.toUpperCase()}</span>;
+  });
+
+  const keyboardElements = alphabet.split("").map((letter) => {
+    const isGuessed = guessedLetters.includes(letter);
+    const isCorrect = isGuessed && currentWord.includes(letter);
+    const isWrong = isGuessed && !currentWord.includes(letter);
+    const className = clsx({
+      "correct": isCorrect,
+      "wrong": isWrong,
+    });
+
+    return (
+      <button onClick={() => addGuessedLetter(letter)} key={letter} className={className}>
+        {letter.toUpperCase()}
+      </button>
+    );
+  });
+
   return (
-    <div className="App">
-      <h1>Assembly: Endgame</h1>
-    </div>
+    <main>
+      <header>
+        <h1>Assembly: Endgame</h1>
+        <p>
+          Guess the word within 8 attempts to keep the programming world safe
+          from Assembly!
+        </p>
+      </header>
+      <section className="game-status">
+        <h2>You win!</h2>
+        <p>Well done! 🎉</p>
+      </section>
+      <section className="language-chips">{languageElements}</section>
+      <section className="word">{letterElements}</section>
+      <section className="keyboard">{keyboardElements}</section>
+      <button className="new-game">New Game</button>
+    </main>
   );
 }
